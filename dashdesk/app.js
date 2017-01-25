64cedcc9ffcea7707797bacae6597ef6b06c53bc
kevinkid <win-bigkevin2682@gmail.com>
2017-01-25 17:48:18 +0300

    cleaning code

--- dashdesk/app.js
+++ dashdesk/app.js
@@ -1,24 +1,29 @@
 ﻿var express = require('express');
+var app = express();
 var http = require('http');
 var path = require('path');
 var bodyParser = require('body-parser');
-
 var favicon = require('serve-favicon');
 var routes = require("./routes/index");
 var listen = require("./routes/listen");
 var logger = require("morgan");
 
 
-var app = express();
-
-
 var env = process.env.NODE_ENV || 'development';
 app.locals.ENV = env;
 app.locals.ENV_DEVELOPMENT = env === 'development';
 
+// Express cors config 
+app.use(function (req, res, next) {
+    req.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Origin", "https://dashdesk.azurewebsites.net");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Credentials", "false");
+    res.header("Origin, Content-Type, Access-Control-Allow-Credentials, Access-Control-Allow-Headers");
+    next();
+});
 
-
-// all environments
+// Port config 
 app.set('port', process.env.PORT || 3000);
 
 app.set('views', path.join(__dirname, 'views'));
@@ -34,11 +39,11 @@ app.use(bodyParser.urlencoded({
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+// Routes config 
 app.use('/', routes);
 app.use('/listen', listen);
 
-// capture 404 errors 
+// Capture 404 errors 
 app.use(function (req, res, next) {
     var error = new error('Not Found ');
     error.status = 404;
@@ -46,7 +51,7 @@ app.use(function (req, res, next) {
 });
 
 
-// error handling 
+// Error Handling  
 if (app.get("env") === "development" ) {
     app.use(function (err, req, res) {
         res.render("error", {
@@ -57,17 +62,6 @@ if (app.get("env") === "development" ) {
     });
 }
 
-
-// production error handling 
-app.use(function (err, req, res) {
-   
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {},
-        title: "Error"
-    });
-});
 
 var server = app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
